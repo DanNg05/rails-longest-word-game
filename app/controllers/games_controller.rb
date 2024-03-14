c9f05fce4cefd30c04a7cfaaae6cba178b5ae512
Dan Nguyen <sydan05071997@gmail.com>
2024-03-14 18:30:08 +1030

    run successfully, need styling

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -11,10 +11,33 @@ class GamesController < ApplicationController
   end
 
   def score
-    @word = params["word"]
-    web = "https://wagon-dictionary.herokuapp.com/#{@word}"
+    @word = params["word"].downcase
+    @grid = params[:array].split(",")
+    # web = "https://wagon-dictionary.herokuapp.com/#{@word}"
+    # user_serialized = URI.open(web).read
+    # user = JSON.parse(user_serialized)
+    if include?(@word, @grid)
+      if check_word(@word)
+        @answer = "well done"
+      else
+        @answer = "Sorry not an English word!"
+      end
+    else
+      @answer = "Not in the grid"
+    end
+    # raise
+  end
+
+  private
+
+  def include?(guess, grid)
+    guess.chars.all? { |letter| guess.count(letter) <= grid.count(letter) }
+  end
+
+  def check_word(word)
+    web = "https://wagon-dictionary.herokuapp.com/#{word}"
     user_serialized = URI.open(web).read
     user = JSON.parse(user_serialized)
-    raise
+    return user["found"]
   end
 end
